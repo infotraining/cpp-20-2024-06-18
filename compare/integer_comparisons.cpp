0341de8233591fe23345418eeb9b94c69f17fecb
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-06-18 10:03:19 +0200

    cmp_*

--- compare/integer_comparisons.cpp
+++ compare/integer_comparisons.cpp
@@ -4,10 +4,33 @@
 #include <string>
 #include <utility>
 
+using namespace std::literals;
+
 TEST_CASE("safe comparing integral numbers")
 {
     int x = -42;
     unsigned int y = 665;
 
-    //CHECK(x < y);
+    SECTION("std::cmp_*")
+    {
+        // CHECK(x < y);
+        CHECK(std::cmp_less(x, y));
+
+        auto cmp_by_less = [](auto a, auto b) {                        
+            if constexpr(std::integral<decltype(a)> && std::integral<decltype(b)>)
+                return std::cmp_less(a, b);
+            else
+                return a < b;
+        };
+
+        CHECK(cmp_by_less(x, y));
+        CHECK(cmp_by_less("abc"s, "def"s));
+    }
+
+    SECTION("std::in_range<T>")
+    {
+        CHECK(std::in_range<size_t>(665));
+        CHECK(std::in_range<size_t>(-1) == false);
+        static_assert(std::in_range<uint8_t>(257) == false);
+    }
 }
